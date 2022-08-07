a977137b9af9a5d6ff3e277f17b0cc595378c69f
Martin Ligtenberg <martin@ligtenberg.digital>
2022-08-07 14:11:42 +0200

    fix copy

--- init.sh
+++ init.sh
@@ -7,10 +7,10 @@ git clone --depth 1 https://github.com/RPI-Distro/pi-gen.git
 touch ./pi-gen/stage2/SKIP_IMAGES ./pi-gen/stage2/SKIP_NOOBS
 
 ## link config
-cp ../config ./pi-gen
+cp ./config ./pi-gen/config
 
 ## link stage2-iotedge
-cp -r ../stage2-iotedge ./pi-gen
+cp -r ./stage2-iotedge ./pi-gen/stage2-iotedge
 
 ## link future deploys
 [[ ! -d ./pi-gen/deploy ]] && mkdir ./pi-gen/deploy
