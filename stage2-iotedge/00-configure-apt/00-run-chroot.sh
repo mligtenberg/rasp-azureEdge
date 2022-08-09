0aa5d27bb0919c43cde3b2af6256431813f4eabe
Martin Ligtenberg <martin@ligtenberg.digital>
2022-08-10 00:32:25 +0200

    fix deamon.json issue

--- stage2-iotedge/00-configure-apt/00-run-chroot.sh
+++ stage2-iotedge/00-configure-apt/00-run-chroot.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -e
 
-wget https://packages.microsoft.com/config/ubuntu/20.04/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
+wget https://packages.microsoft.com/config/debian/11/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
 sudo dpkg -i packages-microsoft-prod.deb
 rm packages-microsoft-prod.deb
--- stage2-iotedge/02-configure-docker/00-run.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash -e
-JSON_STRING='{"log-driver": "local"}'
-echo JSON_STRING > "${ROOTFS_DIR}/etc/docker/daemon.json"
